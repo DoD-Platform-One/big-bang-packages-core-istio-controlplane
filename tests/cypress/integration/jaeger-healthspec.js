50735b45c457a053469997a80e9ed3c1ca5a4d9e
runyontr <tom@runyon.dev>
2021-03-24 17:03:11 +0000

    Remove jaeger

--- tests/cypress/integration/jaeger-healthspec.js
+++
@@ -1,8 +0,0 @@
-describe('Basic Jaeger', function() {
-    it('Check Jaeger page is accessible', function() {
-      cy.visit(Cypress.env('jaeger_url'))
-      cy.title().should('eq', 'Jaeger UI')
-     
-    })
-     
-}) 
