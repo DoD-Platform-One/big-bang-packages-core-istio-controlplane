a29d2ead2c23fe2bc6bf9a3546efb44942fe37c2
Nick Nellis <nick@tetrate.io>
2021-04-19 17:36:30 +0000

    1.8 upgrade

--- tests/cypress/integration/kiali-healthspec.js
+++
@@ -1,8 +0,0 @@
-describe('Basic Kiali', function() {
-    it('Check kiali page is accessible', function() {
-      cy.visit(Cypress.env('kiali_url'))
-      cy.title().should('eq', 'Kiali Console')
-     
-    })
-     
-}) 
